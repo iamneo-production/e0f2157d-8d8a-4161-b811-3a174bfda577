4847aaf655a1a8540412dc3478419ec428af1e71
anushka-21 <mitra.anushka2101@gmail.com>
2023-06-29 13:59:25 +0000

    initial

--- reactapp/src/components/Pages/HotelInfo.jsx
+++ reactapp/src/components/Pages/HotelInfo.jsx
@@ -1,16 +1,1 @@
-// Home
-
-import React from "react";
-
-import Hotels from "./components/Hotels/Hotels";
-
-
-function HotelInfo() {
-    return (
-      <>
-        <Hotels />
-      </>
-    );
-  }
-  
-  export default HotelInfo;
+// HotelHome
