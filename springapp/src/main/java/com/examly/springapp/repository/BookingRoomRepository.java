5b154584dbb1a71667008b28b0321d02fbed5a1d
prasanna-09 <198r1a04a6@gmail.com>
2023-07-25 13:23:37 +0000

    BookingRoom updated

--- springapp/src/main/java/com/examly/springapp/repository/BookingRoomRepository.java
+++ springapp/src/main/java/com/examly/springapp/repository/BookingRoomRepository.java
@@ -6,5 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
+
 public interface BookingRoomRepository extends JpaRepository<BookingRoom, Long> {
 }
