574c2c2b3fe9c3343e4e2432ba5ff7a48cddd371
MohanaVijayaraj <mohana.v.2019.cse@ritchennai.edu.in>
2023-07-29 06:18:22 +0000

    files deleted

--- Springapp/src/main/java/com/examly/springapp/repository/AddHotelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.examly.springapp.repository;
-
-import com.examly.springapp.model.AddHotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AddHotelRepository extends JpaRepository<AddHotel, Long> {
-}
